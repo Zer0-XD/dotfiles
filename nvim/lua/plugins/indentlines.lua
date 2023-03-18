ebab10edb41892d52a28f92727fc3a6d95c7d5be
Zero <58396888+Zer0-XD@users.noreply.github.com>
2023-03-18 11:33:35 +0500

    Update indentlines.lua

--- nvim/lua/plugins/indentlines.lua
+++ nvim/lua/plugins/indentlines.lua
@@ -2,9 +2,13 @@ vim.opt.list = false
 vim.opt.termguicolors = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
+vim.cmd([[highlight IndentBlanklineIndent1 guifg=#1F1F28 gui=nocombine]])
 
 require("indent_blankline").setup({
 	space_char_blankline = " ",
 	show_current_context = true,
 	show_current_context_start = true,
+	char_highlight_list = {
+		"IndentBlanklineIndent1",
+	},
 })
